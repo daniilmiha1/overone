2ccaacc81f5c40d6be74163c2512c0918af379ef
daniilmiha1 <danilamih01@gmail.com>
2021-11-14 11:00:07 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-b = {ethereum: '4600',
-     litecoin: '252'}
+c = {'ethereum': '4600',
+     'litecoin': '252'}
 a = 20
 print(f'My name is Daniil Im {a} years old')
 print('Hello')
 print('My hobby is cryptocurrency')
-print(f'Now price is {c}')
+print(f'Now price of crypto is {c}')
